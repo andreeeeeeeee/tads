3f22ec805db84b176c20d1f9aa1574e93c7f00ae
André Bastilhos <50469267+andreeeeeeeee@users.noreply.github.com>
2024-02-28 21:50:19 -0300

    Add files via upload

---
+++ 2024-1/logica/Lista 1/Eleicao.java
@@ -0,0 +1,47 @@
+/*
+ * 8.
+ * Desenvolva o algoritmo para calcular o resultado de uma eleição de um municipal.
+ * A entrada de dados será:
+    ● Número total de eleitores;
+    ● Número de votos válidos;
+    ● Número de votos brancos;
+    ● Número de votos nulos.
+ * O algoritmo deve calcular e exibir para o usuário as seguintes informações (em
+ * relação ao total de eleitores):
+    ● Percentual de votos válidos;
+    ● Percentual de votos brancos;
+    ● Percentual de votos nulos;
+ */
+
+import java.util.Scanner;
+
+ public class Eleicao {
+    public static void main(String[] args) {
+        // DECLARACAO
+        Scanner in;
+
+        // INICIALIZACAO
+        in = new Scanner(System.in);    // TERMINAL / ENTRADA DO USUARIO
+        
+        // tipo / nomes
+        double eleitores, validos, brancos, nulos, percValidos, percBrancos, percNulos;
+
+        System.out.println("Digite o número total de eleitores:");
+        eleitores = in.nextInt();
+        System.out.println("Digite o número total de votos válidos:");
+        validos = in.nextInt();
+       System.out.println("Digite o número total de votos brancos:");
+        brancos = in.nextInt();
+        System.out.println("Digite o número total de votos nulos:");
+        nulos = in.nextInt();
+
+        percValidos = validos / eleitores * 100;
+        percBrancos = brancos / eleitores * 100;
+        percNulos = nulos / eleitores * 100;
+        
+        System.out.println("Dos " + (int)eleitores + " eleitores:");
+        System.out.println(percValidos + "% (" + (int)validos + ") são válidos;");
+        System.out.println(percBrancos + "% (" + (int)brancos + ") são brancos;");
+        System.out.println(percNulos + "% (" + (int)nulos + ") são nulos;");
+    }
+ }
---
+++ 2024-1/logica/Lista 1/Ingressos.java
@@ -0,0 +1,44 @@
+/*
+ * 10.
+ * Desenvolva um algoritmo que será utilizado para automatizar o cálculo do público e
+ * da renda total de um evento esportivo. Este evento esportivo possui um valor fixo
+ * cobrado por ingresso, no entanto, os sócios do clube em cujas dependências ocorre
+ * o evento possuem um desconto de 30% no valor do ingresso e as crianças menoresde 10 anos não pagam ingresso.
+ * Baseado nos dados acima apresentados o usuário deverá digitar 4 informações de entrada para o sistema, são elas:
+ *  * Valor de cada ingresso
+ *  * Número de pessoas (público do evento) que são sócias do clube
+ *  * Número de pessoas (público do evento) não pagantes (menores de 10 anos)
+ *  * Número de pessoas (público do evento) pagantes (sem desconto algum)
+ * O algoritmo deverá calcular e exibir o público total do evento, a renda total do evento
+ * e o valor que deixou de ser arrecadada devido aos descontos e isenções.
+ */
+
+import java.util.Scanner;
+
+public class Ingressos {
+    public static void main(String[] main) {
+        // DECLARACAO
+        Scanner in;
+
+        // INICIALIZACAO
+        in = new Scanner(System.in);    // TERMINAL / ENTRADA DO USUARIO
+        
+        // tipo / nomes
+        double valorIngresso, valorDesconto, totalRenda, totalIngressos, totalDesconto;
+        int pagantes, naoPagantes, socios, totalPublico;
+
+        System.out.println("Valor de cada ingresso, em reais:");
+        valorIngresso = in.nextDouble();
+        valorDesconto = valorIngresso * 0.3;
+        System.out.println("Número de pessoas (público do evento) que são sócias do clube:");
+        socios = in.nextInt();
+        System.out.println("Número de pessoas (público do evento) não pagantes (menores de 10 anos):");
+        naoPagantes = in.nextInt();
+        System.out.println("Número de pessoas (público do evento) pagantes (sem desconto algum):");
+        pagantes = in.nextInt();
+
+        totalPublico = socios + naoPagantes + pagantes;
+        totalIngressos = totalPublico * valorIngresso;
+        totalRenda = socios * valorIngresso + pagantes * (valorIngresso - valorDesconto);
+    }
+}
