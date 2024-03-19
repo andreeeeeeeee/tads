5e07a10a5938457f006bcce17ca571b66baa22e7
André Bastilhos <50469267+andreeeeeeeee@users.noreply.github.com>
2024-03-18 21:05:22 -0300

    Update Conversao.java

--- 2024-1/logica/Mini Lista Complementar 1/Conversao.java
+++ 2024-1/logica/Mini Lista Complementar 1/Conversao.java
@@ -12,19 +12,19 @@ public class Conversao {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int inteiro;
-        float decimal;
+        int inteiro, decInteiro;
+        float decimal, intDecimal;
 
         System.out.println("Digite um número inteiro:");
         inteiro = in.nextInt();
         System.out.println("Digite um número decimal:");
         decimal = in.nextFloat();
 
-        decimal = (float) inteiro;
-        inteiro = (int) decimal;
+        intDecimal = (float) inteiro;
+        decInteiro = (int) decimal;
 
-        System.out.println("Número inteiro convertido em decimal: " + decimal);
-        System.out.println("Número decimal convertido em inteiro: " + inteiro);
+        System.out.println("Número inteiro convertido em decimal: " + intDecimal);
+        System.out.println("Número decimal convertido em inteiro: " + decInteiro);
 
         in.close();
     }
