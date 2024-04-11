bd540ab412b01d2161601752d641eb8a439f5432
André Bastilhos <50469267+andreeeeeeeee@users.noreply.github.com>
2024-04-11 19:31:59 -0300

    Create Media.java

---
+++ 2024-1/logica/Atividades 04-11/Media.java
@@ -0,0 +1,28 @@
+/*
+ * 1.
+ * Calcule a média simples entre 5 valores digitados pelo usuário
+ *  -   Não devem ser declaradas 5 variáveis de valor
+ */
+
+
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int soma = 0, i = 0;
+
+        while (i < 5) {
+            i++;
+            System.out.printf("Digite o %d° valor: ", i);
+            int numero = in.nextInt();
+            soma += numero;
+        }
+        in.close();
+
+        float media = soma / 5f;
+
+        System.out.printf("A média entre os 5 valores é de %.2f\n", media);
+    }
+}
