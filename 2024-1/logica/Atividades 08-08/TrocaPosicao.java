67f445caa03b5d53cef7c7800d08c40049d0a294
André Bastilhos <50469267+andreeeeeeeee@users.noreply.github.com>
2024-08-08 19:34:13 -0300

    Create TrocaPosicao.java

---
+++ 2024-1/logica/Atividades 08-08/TrocaPosicao.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class TrocaPosicao {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int [] v = {21,45,231,45,21,87,65,7,4,5,45,2,84,21,85,3,84,6,645,8};
+
+        while (true) {
+            System.out.print("[ ");
+            for (int i = 0; i < v.length; i++) {
+                System.out.print(v[i] + " ");
+            }
+            System.out.print("]\n");
+
+            System.out.print("Digite a primeira posição: ");
+            int posicao1 = in.nextInt();
+            System.out.print("Digite a segunda posição: ");
+            int posicao2 = in.nextInt();
+
+            System.out.println();
+
+            if (posicao1 >=0 && posicao1 <v.length && posicao2 >=0 && posicao2 <v.length) {
+                int valor1 = v[posicao1];
+                int valor2=v[posicao2];
+
+                v[posicao1] = valor2;
+                v[posicao2] = valor1;
+            } else
+                break;
+        }
+
+        in.close();
+    }
+}
