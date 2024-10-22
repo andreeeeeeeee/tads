097612354ce1e439dc7232c0db42e017cb4a1540
André Bastilhos <50469267+andreeeeeeeee@users.noreply.github.com>
2024-10-22 20:59:58 -0300

    Add files via upload

---
+++ 2024-2/programacao_modular/10.22/salsichao.js
@@ -0,0 +1,97 @@
+function substring(string, start, end = string.length) {
+  let s = "";
+
+  for (let i = start; i < string.length && i < end; i++) s += string[i];
+
+  return s;
+}
+
+function split(string, separator) {
+  let a = [];
+  let start = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == separator) {
+      let substr = substring(string, start, i);
+      a.push(substr);
+      start = i + 1;
+    }
+
+    if (i == string.length - 1) {
+      let substr = substring(string, start);
+      a.push(substr);
+    }
+  }
+  return a;
+}
+
+function join(array, separator) {
+  let s = "";
+
+  for (let i = 0; i < array.length; i++) {
+    if (i == array.length - 1) {
+      s += array[i];
+    } else {
+      s += array[i] + separator;
+    }
+  }
+
+  return s;
+}
+
+function camelCase(string) {
+  const a = split(string, " ");
+
+  for (let i = 1; i < a.length; i++)
+    a[i] = a[i][0].toUpperCase() + substring(a[i], 1);
+
+  const s = join(a, "");
+  return s;
+}
+
+function PascalCase(string) {
+  const a = split(string, " ");
+
+  for (let i = 0; i < a.length; i++)
+    a[i] = a[i][0].toUpperCase() + substring(a[i], 1);
+
+  const s = join(a, "");
+  return s;
+}
+
+function snake_case(string) {
+  const a = split(string, " ");
+
+  for (let i = 0; i < a.length; i++) {
+    a[i] = a[i].toLowerCase();
+  }
+
+  const s = join(a, "_");
+  return s;
+}
+
+function kebabcase(string) {
+  const a = split(string, " ");
+
+  for (let i = 0; i < a.length; i++) {
+    a[i] = a[i].toLowerCase();
+  }
+
+  const s = join(a, "-");
+  return s;
+}
+
+const cargo = "desenvolvedora";
+console.log(substring(cargo, 5, 10));
+
+const disciplina = "progamação modular tads";
+console.log(split(disciplina, " "));
+
+const nomeSobrenome = ["Gabriel", "Jacinto"];
+console.log(join(nomeSobrenome, " "));
+
+const salsichaocase = "salsichao case";
+console.log(camelCase(salsichaocase));
+console.log(PascalCase(salsichaocase));
+console.log(snake_case(salsichaocase));
+console.log(kebabcase(salsichaocase));
